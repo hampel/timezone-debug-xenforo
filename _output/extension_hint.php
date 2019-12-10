85cd8f6b88049c5a3baa457b6db43fc0adba587d
Simon Hampel <simon@hampelgroup.com>
2019-12-10 12:57:10 +1100

    remove dev output from master branch

--- _output/extension_hint.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ################## THIS IS A GENERATED FILE ##################
-// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
-
-namespace Hampel\TimeZoneDebug\XF\Admin\Controller
-{
-	class XFCP_Tools extends \XF\Admin\Controller\Tools {}
-}
